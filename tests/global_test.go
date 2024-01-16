5aad394127132cdaf8e807ccffd9629d5861a70b
dkool <dkool@dkools-MacBook-Pro.local>
2024-01-16 20:40:32 +0100

    fix path using github builtin variables

--- tests/global_test.go
+++ tests/global_test.go
@@ -169,7 +169,7 @@ func ExtractReadmeResources(data string) ([]string, error) {
 func ExtractTerraformResources() ([]string, error) {
 	parser := hclparse.NewParser()
 	// file, diags := parser.ParseHCLFile("../main.tf")
-	file, diags := parser.ParseHCLFile("../../../caller/main.tf")
+	file, diags := parser.ParseHCLFile(os.Getenv("GITHUB_WORKSPACE") + "/caller/main.tf")
 	if diags.HasErrors() {
 		return nil, errors.New(diags.Error())
 	}
