93daa7f1f6643ed50223e1f43db2da9b03814c58
dkool <dkool@dkools-MacBook-Pro.local>
2024-07-30 12:35:48 +0200

    refactor markdown tests

--- tests/global_test.go
+++ tests/global_test.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -17,10 +17,47 @@ import (
 	"mvdan.cc/xurls/v2"
 )
 
-type MarkdownValidation struct {
-	ReadmePath string
-	Data       string
+type Validator interface {
+	Validate() []error
 }
+
+type SectionValidator interface {
+	ValidateSection(data string) []error
+}
+
+type FileValidator interface {
+	ValidateFile(filePath string) []error
+}
+
+type URLValidator interface {
+	ValidateURLs(data string) []error
+}
+
+type TerraformValidator interface {
+	ValidateTerraformDefinitions(data string) []error
+}
+
+type MarkdownValidator struct {
+	readmePath   string
+	data         string
+	sections     []SectionValidator
+	files        []FileValidator
+	urlValidator URLValidator
+	tfValidator  TerraformValidator
+}
+
+type Section struct {
+	Header  string
+	Columns []string
+}
+
+type RequiredFile struct {
+	Name string
+}
+
+type StandardURLValidator struct{}
+
+type TerraformDefinitionValidator struct{}
 
 type TerraformConfig struct {
 	Resource []Resource `hcl:"resource,block"`
@@ -39,216 +76,273 @@ type Data struct {
 	Properties hcl.Body `hcl:",remain"`
 }
 
-func InitMarkdownTests(readmePath string) (*MarkdownValidation, error) {
-	data, err := os.ReadFile(readmePath)
+func NewMarkdownValidator(readmePath string) (*MarkdownValidator, error) {
+	testsDir, err := os.Getwd()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get current working directory: %v", err)
+	}
+
+	rootDir := filepath.Dir(testsDir)
+	absReadmePath := filepath.Join(rootDir, readmePath)
+
+	data, err := os.ReadFile(absReadmePath)
 	if err != nil {
 		return nil, err
 	}
 
-	return &MarkdownValidation{
-		ReadmePath: readmePath,
-		Data:       string(data),
+	sections := []SectionValidator{
+		Section{Header: "Goals"},
+		Section{Header: "Resources", Columns: []string{"Name", "Type"}},
+		Section{Header: "Providers", Columns: []string{"Name", "Version"}},
+		Section{Header: "Requirements", Columns: []string{"Name", "Version"}},
+		Section{Header: "Inputs", Columns: []string{"Name", "Description", "Type", "Default", "Required"}},
+		Section{Header: "Outputs", Columns: []string{"Name", "Description"}},
+		Section{Header: "Features"},
+		Section{Header: "Testing"},
+		Section{Header: "Authors"},
+		Section{Header: "License"},
+	}
+
+	files := []FileValidator{
+		RequiredFile{Name: filepath.Join(rootDir, "README.md")},
+		RequiredFile{Name: filepath.Join(rootDir, "CONTRIBUTE.md")},
+		RequiredFile{Name: filepath.Join(rootDir, "LICENSE")},
+	}
+
+	return &MarkdownValidator{
+		readmePath:   absReadmePath,
+		data:         string(data),
+		sections:     sections,
+		files:        files,
+		urlValidator: StandardURLValidator{},
+		tfValidator:  TerraformDefinitionValidator{},
 	}, nil
 }
 
-func (ts *MarkdownValidation) ValidateURLs(t *testing.T) {
-	rxStrict := xurls.Strict()
-	urls := rxStrict.FindAllString(ts.Data, -1)
+func (mv *MarkdownValidator) Validate() []error {
+	var allErrors []error
 
-	var wg sync.WaitGroup
-	for _, u := range urls {
-		if strings.Contains(u, "registry.terraform.io/providers/") {
-			continue
+	allErrors = append(allErrors, mv.ValidateSections()...)
+	allErrors = append(allErrors, mv.ValidateFiles()...)
+	allErrors = append(allErrors, mv.ValidateURLs()...)
+	allErrors = append(allErrors, mv.ValidateTerraformDefinitions()...)
+
+	return allErrors
+}
+
+func (mv *MarkdownValidator) ValidateSections() []error {
+	var allErrors []error
+	for _, section := range mv.sections {
+		allErrors = append(allErrors, section.ValidateSection(mv.data)...)
+	}
+	return allErrors
+}
+
+func (s Section) ValidateSection(data string) []error {
+	var errors []error
+	tableHeaderRegex := `^\s*\|(.+?)\|\s*(\r?\n)`
+
+	flexibleHeaderPattern := regexp.MustCompile(`(?mi)^\s*##\s+` + strings.Replace(regexp.QuoteMeta(s.Header), `\s+`, `\s+`, -1) + `s?\s*$`)
+	headerLoc := flexibleHeaderPattern.FindStringIndex(data)
+
+	if headerLoc == nil {
+		errors = append(errors, formatError("incorrect header: expected '## %s', found 'not present'", s.Header))
+	} else {
+		actualHeader := strings.TrimSpace(data[headerLoc[0]:headerLoc[1]])
+		if actualHeader != "## "+s.Header {
+			errors = append(errors, formatError("incorrect header: expected '## %s', found '%s'", s.Header, actualHeader))
 		}
 
-		_, err := url.Parse(u)
-		if err != nil {
+		if len(s.Columns) > 0 {
+			startIdx := headerLoc[1]
+			dataSlice := data[startIdx:]
+
+			tableHeaderPattern := regexp.MustCompile(tableHeaderRegex)
+			tableHeaderMatch := tableHeaderPattern.FindStringSubmatch(dataSlice)
+			if tableHeaderMatch == nil {
+				errors = append(errors, formatError("missing table after header: %s", actualHeader))
+			} else {
+				actualHeaders := parseHeaders(tableHeaderMatch[1])
+				if !equalSlices(actualHeaders, s.Columns) {
+					errors = append(errors, compareColumns(s.Header, s.Columns, actualHeaders))
+				}
+			}
+		}
+	}
+
+	return errors
+}
+
+func (mv *MarkdownValidator) ValidateFiles() []error {
+	var allErrors []error
+	for _, file := range mv.files {
+		allErrors = append(allErrors, file.ValidateFile(file.(RequiredFile).Name)...)
+	}
+	return allErrors
+}
+
+func (rf RequiredFile) ValidateFile(filePath string) []error {
+	var errors []error
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			errors = append(errors, formatError("file does not exist:\n  %s", filePath))
+		} else {
+			errors = append(errors, formatError("error accessing file:\n  %s\n  %v", filePath, err))
+		}
+		return errors
+	}
+
+	if fileInfo.Size() == 0 {
+		errors = append(errors, formatError("file is empty:\n  %s", filePath))
+	}
+
+	return errors
+}
+
+func (mv *MarkdownValidator) ValidateURLs() []error {
+	return mv.urlValidator.ValidateURLs(mv.data)
+}
+
+func (suv StandardURLValidator) ValidateURLs(data string) []error {
+	rxStrict := xurls.Strict()
+	urls := rxStrict.FindAllString(data, -1)
+
+	var wg sync.WaitGroup
+	errChan := make(chan error, len(urls))
+
+	for _, u := range urls {
+		if strings.Contains(u, "registry.terraform.io/providers/") {
 			continue
 		}
 
 		wg.Add(1)
 		go func(url string) {
 			defer wg.Done()
-
-			resp, err := http.Get(url)
-			if err != nil {
-				t.Errorf("Failed: URL: %s, Error: %v", url, err)
-				return
-			}
-			defer resp.Body.Close()
-
-			if resp.StatusCode != http.StatusOK {
-				t.Errorf("Failed: URL: %s, Status code: %d", url, resp.StatusCode)
+			if err := suv.validateSingleURL(url); err != nil {
+				errChan <- err
 			}
 		}(u)
 	}
+
 	wg.Wait()
-}
+	close(errChan)
 
-func (ts *MarkdownValidation) ValidateReadmeHeaders(t *testing.T) {
-	requiredHeaders := []string{
-		"## Goals",
-		"## Resources",
-		"## Providers",
-		"## Requirements",
-		"## Inputs",
-		"## Outputs",
-		"## Features",
-		"## Testing",
-		"## Authors",
-		"## License",
+	var errors []error
+	for err := range errChan {
+		errors = append(errors, err)
 	}
 
-	for _, requiredHeader := range requiredHeaders {
-		if !strings.Contains(ts.Data, requiredHeader) {
-			t.Errorf("Failed: README.md does not contain required header '%s'", requiredHeader)
-		}
-	}
+	return errors
 }
 
-func (ts *MarkdownValidation) ValidateReadmeNotEmpty(t *testing.T) {
-	if len(ts.Data) == 0 {
-		t.Errorf("Failed: README.md is empty.")
+func (suv StandardURLValidator) validateSingleURL(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return formatError("error accessing URL:\n  %s\n  %v", url, err)
 	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return formatError("URL returned non-OK status:\n  %s\n  Status: %d", url, resp.StatusCode)
+	}
+
+	return nil
 }
 
-func (ts *MarkdownValidation) ValidateTableHeaders(t *testing.T, header string, columns []string) {
-	requiredHeaders := []string{"## " + header}
-	for _, requiredHeader := range requiredHeaders {
-		headerPattern := regexp.MustCompile("(?m)^" + regexp.QuoteMeta(requiredHeader) + "\\s*$")
-		headerLoc := headerPattern.FindStringIndex(ts.Data)
-		if headerLoc == nil {
-			t.Errorf("Failed: README.md does not contain required header '%s'", requiredHeader)
-			continue
-		}
-
-		tablePattern := regexp.MustCompile(`(?s)` + regexp.QuoteMeta(requiredHeader) + `(\s*\|.*\|)+\s*`)
-		tableLoc := tablePattern.FindStringIndex(ts.Data)
-		if tableLoc == nil {
-			t.Errorf("Failed: README.md does not contain a table immediately after the header '%s'", requiredHeader)
-			continue
-		}
-
-		columnHeaders := strings.Join(columns, " \\| ")
-		headerRowPattern := regexp.MustCompile(`(?m)\| ` + columnHeaders + ` \|`)
-		headerRowLoc := headerRowPattern.FindStringIndex(ts.Data[tableLoc[0]:tableLoc[1]])
-		if headerRowLoc == nil {
-			t.Errorf(
-				"Failed: README.md does not contain the correct column names in the table under header '%s'",
-				requiredHeader,
-			)
-		}
-	}
+func (mv *MarkdownValidator) ValidateTerraformDefinitions() []error {
+	return mv.tfValidator.ValidateTerraformDefinitions(mv.data)
 }
 
-func ExtractReadmeResources(data string) ([]string, error) {
+func (tdv TerraformDefinitionValidator) ValidateTerraformDefinitions(data string) []error {
+	tfResources, err := extractTerraformResources()
+	if err != nil {
+		return []error{err}
+	}
+
+	readmeResources, err := extractReadmeResources(data)
+	if err != nil {
+		return []error{err}
+	}
+
+	var errors []error
+
+	missingInMarkdown := findMissingItems(tfResources, readmeResources)
+	if len(missingInMarkdown) > 0 {
+		errors = append(errors, formatError("missing in markdown:\n  %s", strings.Join(missingInMarkdown, "\n  ")))
+	}
+
+	missingInCode := findMissingItems(readmeResources, tfResources)
+	if len(missingInCode) > 0 {
+		errors = append(errors, formatError("missing in code:\n  %s", strings.Join(missingInCode, "\n  ")))
+	}
+
+	return errors
+}
+
+func extractReadmeResources(data string) ([]string, error) {
 	var resources []string
-	lines := strings.Split(data, "\n")
-	inResourcesTable := false
+	resourcesPattern := regexp.MustCompile(`(?s)## Resources.*?\n(.*?)\n##`)
+	resourcesSection := resourcesPattern.FindStringSubmatch(data)
+	if len(resourcesSection) < 2 {
+		return nil, errors.New("resources section not found or empty")
+	}
 
-	for _, line := range lines {
-		if strings.TrimSpace(line) == "## Resources" {
-			inResourcesTable = true
-			continue
+	linePattern := regexp.MustCompile(`\| \[([^\]]+)\]\([^\)]+\) \| [^\|]+\|`)
+	matches := linePattern.FindAllStringSubmatch(resourcesSection[1], -1)
+
+	for _, match := range matches {
+		if len(match) > 1 {
+			resources = append(resources, strings.TrimSpace(match[1]))
 		}
+	}
 
-		if inResourcesTable && strings.HasPrefix(line, "## ") {
-			break
-		}
+	return resources, nil
+}
 
-		if inResourcesTable {
-			if strings.Contains(line, "| [") {
-				parts := strings.Split(line, "|")
-				if len(parts) > 2 {
-					resourceLink := strings.TrimSpace(parts[1])
-					start := strings.Index(resourceLink, "[")
-					end := strings.Index(resourceLink, "](")
-					if start != -1 && end != -1 && start < end {
-						resourceName := resourceLink[start+1 : end]
-						resources = append(resources, resourceName)
-					}
+func extractTerraformResources() ([]string, error) {
+	var resources []string
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get current working directory: %v", err)
+	}
+	rootDir := filepath.Dir(cwd)
+
+	dirsToSearch := []string{rootDir}
+
+	modulesDir := filepath.Join(rootDir, "modules")
+	if _, err := os.Stat(modulesDir); !os.IsNotExist(err) {
+		dirsToSearch = append(dirsToSearch, modulesDir)
+	}
+
+	for _, dir := range dirsToSearch {
+		err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				if os.IsNotExist(err) {
+					return nil
 				}
+				return err
 			}
-		}
-	}
-	return resources, nil
-}
-
-//func ExtractTerraformResources() ([]string, error) {
-//parser := hclparse.NewParser()
-//file, diags := parser.ParseHCLFile(os.Getenv("GITHUB_WORKSPACE") + "/caller/main.tf")
-//if diags.HasErrors() {
-//return nil, errors.New(diags.Error())
-//}
-
-//var config TerraformConfig
-//diags = gohcl.DecodeBody(file.Body, nil, &config)
-//if diags.HasErrors() {
-//return nil, errors.New(diags.Error())
-//}
-
-//var resources []string
-//for _, resource := range config.Resource {
-//resources = append(resources, resource.Type)
-//}
-
-//for _, data := range config.Data {
-//resources = append(resources, data.Type)
-//}
-
-//return resources, nil
-//}
-
-func ExtractTerraformResources() ([]string, error) {
-	var resources []string
-
-	// Extract from specific main.tf
-	specificResources, err := extractFromFilePath(os.Getenv("GITHUB_WORKSPACE") + "/caller/main.tf")
-	if err != nil {
-		return nil, err
-	}
-	resources = append(resources, specificResources...)
-
-	// Recursive extraction from modules directory
-	modulesResources, err := extractRecursively(os.Getenv("GITHUB_WORKSPACE") + "/caller/modules")
-	if err != nil {
-		return nil, err
-	}
-	resources = append(resources, modulesResources...)
-
-	return resources, nil
-}
-
-func extractRecursively(dirPath string) ([]string, error) {
-	var resources []string
-
-	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
-		return resources, nil
-	} else if err != nil {
-		return nil, err
-	}
-
-	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.Mode().IsRegular() && filepath.Base(path) == "main.tf" {
+			if info.IsDir() || filepath.Ext(path) != ".tf" {
+				return nil
+			}
 			fileResources, err := extractFromFilePath(path)
 			if err != nil {
 				return err
 			}
 			resources = append(resources, fileResources...)
+			return nil
+		})
+
+		if err != nil {
+			return nil, fmt.Errorf("error walking the path %q: %v", dir, err)
 		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
 	}
 
 	return resources, nil
 }
 
-// extractFromFilePath extracts resources from a given file path.
 func extractFromFilePath(filePath string) ([]string, error) {
 	parser := hclparse.NewParser()
 	file, diags := parser.ParseHCLFile(filePath)
@@ -262,63 +356,85 @@ func extractFromFilePath(filePath string) ([]string, error) {
 		return nil, errors.New(diags.Error())
 	}
 
-	var fileResources []string
+	var resources []string
 	for _, resource := range config.Resource {
-		fileResources = append(fileResources, resource.Type)
+		resources = append(resources, resource.Type)
 	}
-
 	for _, data := range config.Data {
-		fileResources = append(fileResources, data.Type)
+		resources = append(resources, data.Type)
 	}
 
-	return fileResources, nil
+	return resources, nil
 }
 
-func (ts *MarkdownValidation) ValidateTerraformDefinitions(t *testing.T) {
-	tfResources, err := ExtractTerraformResources()
-	if err != nil {
-		t.Errorf("Failed to extract terraform resources: %v", err)
-	}
-
-	readmeResources, err := ExtractReadmeResources(ts.Data)
-	if err != nil {
-		t.Errorf("Failed to extract resources from markdown: %v", err)
-	}
-
-	for _, resource := range tfResources {
-		if !contains(readmeResources, resource) {
-			t.Errorf("%s not found in markdown", resource)
-		}
-	}
-
-	for _, resource := range readmeResources {
-		if !contains(tfResources, resource) {
-			t.Errorf("%s not found in code", resource)
-		}
-	}
+func formatError(format string, args ...interface{}) error {
+	return fmt.Errorf(format, args...)
 }
 
-func contains(slice []string, item string) bool {
-	for _, s := range slice {
-		if s == item {
-			return true
+func parseHeaders(headerRow string) []string {
+	headers := strings.Split(strings.TrimSpace(headerRow), "|")
+	for i, header := range headers {
+		headers[i] = strings.TrimSpace(header)
+	}
+	return headers
+}
+
+func equalSlices(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
 		}
 	}
-	return false
+	return true
+}
+
+func findMissingItems(a, b []string) []string {
+	mb := make(map[string]struct{}, len(b))
+	for _, x := range b {
+		mb[x] = struct{}{}
+	}
+	var diff []string
+	for _, x := range a {
+		if _, found := mb[x]; !found {
+			diff = append(diff, x)
+		}
+	}
+	return diff
+}
+
+func compareColumns(header string, expected, actual []string) error {
+	var mismatchedColumns []string
+	for i := 0; i < len(expected) || i < len(actual); i++ {
+		expectedCol := ""
+		actualCol := ""
+		if i < len(expected) {
+			expectedCol = expected[i]
+		}
+		if i < len(actual) {
+			actualCol = actual[i]
+		}
+		if expectedCol != actualCol {
+			mismatchedColumns = append(mismatchedColumns, fmt.Sprintf("expected '%s', found '%s'", expectedCol, actualCol))
+		}
+	}
+	return formatError("table under header: %s has incorrect column names:\n  %s", header, strings.Join(mismatchedColumns, "\n  "))
 }
 
 func TestMarkdown(t *testing.T) {
 	readmePath := os.Getenv("README_PATH")
-	suite, err := InitMarkdownTests(readmePath)
+
+	validator, err := NewMarkdownValidator(readmePath)
 	if err != nil {
-		t.Fatalf("Failed to create test suite: %v", err)
+		t.Fatalf("Failed to create validator: %v", err)
 	}
 
-	t.Run("URLs", suite.ValidateURLs)
-	t.Run("Headers", suite.ValidateReadmeHeaders)
-	t.Run("NotEmpty", suite.ValidateReadmeNotEmpty)
-	t.Run("ResourceTableHeaders", func(t *testing.T) {
-		suite.ValidateTableHeaders(t, "Resources", []string{"Name", "Type"})
-	})
-	t.Run("TerraformDefinitions", suite.ValidateTerraformDefinitions)
+	errors := validator.Validate()
+	if len(errors) > 0 {
+		for _, err := range errors {
+			t.Errorf("Validation error: %v", err)
+		}
+	}
 }
