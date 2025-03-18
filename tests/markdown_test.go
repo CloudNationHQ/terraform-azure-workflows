06cb2cdd30e0e3b53f75592f596bbaf8c990a4d5
dkooll <dennis.kool@cloudnation.nl>
2025-03-18 14:45:26 +0100

    feat: change additional optional sections and files

--- tests/markdown_test.go
+++ tests/markdown_test.go
@@ -9,10 +9,10 @@ func TestReadmeValidation(t *testing.T) {
 
 	// Use functional options pattern
 	validator, err := markparsr.NewReadmeValidator(
-		markparsr.WithAdditionalSections("Goals", "Non-Goals",
+		markparsr.WithAdditionalSections("Goals",
 			"Features", "Authors", "Contributing", "Testing", "Notes", "References"),
 		markparsr.WithAdditionalFiles("CONTRIBUTING.md", "TESTING.md",
-			"CODE_OF_CONDUCT.md", "SECURITY.md"),
+			"CODE_OF_CONDUCT.md", "SECURITY.md", "GOALS.md"),
 	)
 
 	if err != nil {
