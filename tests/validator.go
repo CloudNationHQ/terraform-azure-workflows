ee74f02d0df4b59f809ce4db10ab3c93e005e1a9
dkooll <dennis.kool@cloudnation.nl>
2025-08-29 13:16:29 +0200

    slightly change package consumption validor

--- tests/validator.go
+++ tests/validator.go
@@ -4,12 +4,15 @@ import (
 	"github.com/cloudnationhq/az-cn-go-validor"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	// Change to repo directory for module detection
 	if repoPath := os.Getenv("REPO_PATH"); repoPath != "" {
-		if err := os.Chdir(repoPath); err != nil {
+		// Go up one level to get to the actual repo directory
+		actualRepoPath := filepath.Dir(repoPath)
+		if err := os.Chdir(actualRepoPath); err != nil {
 			log.Fatal("Failed to change to repo directory:", err)
 		}
 	}
