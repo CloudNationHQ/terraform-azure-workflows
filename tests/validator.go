bc05ab90075edf2213d802327632789ab803928b
dkooll <dennis.kool@cloudnation.nl>
2025-08-29 12:57:14 +0200

    fix wrong validador references in workflow

--- tests/validator.go
+++ tests/validator.go
@@ -3,10 +3,16 @@ package main
 import (
 	"github.com/cloudnationhq/az-cn-go-validor"
 	"log"
+	"os"
 )
 
 func main() {
-	if err := validor.ValidateLocalChanges("examples"); err != nil {
+	examplesPath := "examples"
+	if len(os.Args) > 1 {
+		examplesPath = os.Args[1]
+	}
+
+	if err := validor.ValidateLocalChanges(examplesPath); err != nil {
 		log.Fatal(err)
 	}
 }
