ef3f405134541a55fee19903e8cfea90b27b069b
dkooll <dennis.kool@cloudnation.nl>
2025-08-29 13:12:39 +0200

    slightly change package consumption validor

--- tests/validator.go
+++ tests/validator.go
@@ -7,6 +7,13 @@ import (
 )
 
 func main() {
+	// Change to repo directory for module detection
+	if repoPath := os.Getenv("REPO_PATH"); repoPath != "" {
+		if err := os.Chdir(repoPath); err != nil {
+			log.Fatal("Failed to change to repo directory:", err)
+		}
+	}
+
 	examplesPath := os.Getenv("EXAMPLES_PATH")
 	if examplesPath == "" {
 		examplesPath = "examples"
